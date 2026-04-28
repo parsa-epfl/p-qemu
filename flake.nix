f93a9fd6012d0b3663553205c71c04a8a235d768
cyan.lin <xusine@outlook.com>
2026-04-28 21:19:31 +0200

    chore: Update the build system to use bxdb

--- flake.nix
+++ flake.nix
@@ -4,11 +4,11 @@
   # Each time this url is changed, please rerun `nix flake lock --update-input nixpkgs` to update the lock file
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }: 
+  outputs = { self, nixpkgs }:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
-  in 
+  in
   {
 
     devShells.${system}.default = pkgs.stdenv.mkDerivation {
@@ -35,11 +35,11 @@
       # This property is not required by mkDerivation, but appears as a environmental variable.
       # So I can run $configurationPhase in the shell.
       configurationPhase = ''
-        ./configure --target-list=aarch64-softmmu --disable-gtk --enable-capstone
+        ./configure --target-list=aarch64-softmmu --disable-gtk --enable-capstone --with-bxdb=../bxdb
       '';
 
       configurationPhaseWithExtSnapshots = ''
-        ./configure --target-list=aarch64-softmmu --disable-gtk --enable-capstone --enable-snapext
+        ./configure --target-list=aarch64-softmmu --disable-gtk --enable-capstone --enable-snapext --with-bxdb=../bxdb
       '';
 
       # Also this one.
